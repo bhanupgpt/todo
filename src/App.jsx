6770b1b3285a0a28f11da2263afbf07b6b831b20
ponybo-y <98256435+ponybo-y@users.noreply.github.com>
2024-03-02 19:07:56 +0530

    initializing the app

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+
+import { useState } from "react"
+import "./styles.css"
+
+export default function App() {
+  const [newItem, setNewItem] = useState("")
+  return <>
+    <form className="new-item-form">
+      <div className="form-row">
+        <label htmlFor="item">New Item</label>
+        <input type="text" id = "item" />
+      </div>
+      <button className="btn">Add</button>
+    </form>
+    <h1 className="header">ToDo List</h1>
+    <ul className="list">
+      <li>
+        <label htmlFor="">
+          <input type="checkbox" />
+          item 1
+        </label>
+        <button className="btn btn-danger">Delete</button>
+      </li>
+      <li>
+        <label htmlFor="">
+          <input type="checkbox" />
+          item 1
+        </label>
+        <button className="btn btn-danger">Delete</button>
+      </li>
+    </ul>
+    </>
+
+}
